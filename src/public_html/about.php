bc371e5ebab3a811d8c41b6e2ef77ea431518189
Daniel Sirz 🚬 <Sirz_Daniel@cat.com>
2022-12-10 11:27:10 +0100

    chore: text change to test deploy filter

--- src/public_html/about.php
+++ src/public_html/about.php
@@ -7,7 +7,7 @@ includeFileWithVariables("includes/header.php", array(
 )); ?>
 
 <main id="maincontent" class="content-container">
-    <h1>Proud to be in the cloud!</h1>
+    <h1>Proud to be in the cloud</h1>
     <p>this is what it is all about - being proud to be in the cloud!</p>
     <p>Since day one when i used my computer for something meaningful it was dedicated to the web!</p>
     <img src="img/Profile_Wakeboard.png" alt="Daniel Sirz Wakeboarding" />
