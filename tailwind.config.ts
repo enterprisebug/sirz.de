d53efd99db8e287cdacb2533d059185842811c7d
Daniel Sirz <daniel.sirz@gmail.com>
2024-03-30 14:27:33 +0100

    chore: wip

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,7 @@
+import typographyPlugin from '@tailwindcss/typography'
 import type { Config } from "tailwindcss";
+
+import typographyStyles from './typography'
 
 const config: Config = {
   content: [
@@ -6,7 +9,25 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  // darkMode: 'selector',
+  plugins: [typographyPlugin],
   theme: {
+    fontSize: {
+      xs: ['0.8125rem', { lineHeight: '1.5rem' }],
+      sm: ['0.875rem', { lineHeight: '1.5rem' }],
+      base: ['1rem', { lineHeight: '1.75rem' }],
+      lg: ['1.125rem', { lineHeight: '1.75rem' }],
+      xl: ['1.25rem', { lineHeight: '2rem' }],
+      '2xl': ['1.5rem', { lineHeight: '2rem' }],
+      '3xl': ['1.875rem', { lineHeight: '2.25rem' }],
+      '4xl': ['2rem', { lineHeight: '2.5rem' }],
+      '5xl': ['3rem', { lineHeight: '3.5rem' }],
+      '6xl': ['3.75rem', { lineHeight: '1' }],
+      '7xl': ['4.5rem', { lineHeight: '1' }],
+      '8xl': ['6rem', { lineHeight: '1' }],
+      '9xl': ['8rem', { lineHeight: '1' }],
+    },
+    typography: typographyStyles,
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
@@ -15,6 +36,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
 };
 export default config;
