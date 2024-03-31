05bd8134992272b582ce31f2ede9ded89fa9fd6d
Daniel Sirz <daniel.sirz@gmail.com>
2024-03-31 11:06:37 +0200

    feat: deploy to vercel + mdx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
       'Daniel Sirz - father, Cloud Software architect, Tennis player, Beach-Volleyball player',
   },
   description:
-    'I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms.',
+    'I’m Daniel Sirz. Cloud software architect at Caterpillar Energy Solutions GmbH in Mannheim.',
   alternates: {
     types: {
       // 'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
